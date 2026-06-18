7e968d159b2350a1ff8e322d39dc3cf53cbaa78a
Gargallo-Honesto <rafaelgargallo018@gmail.com>
2026-06-18 23:59:58 +0800

    fixed dashboard cards

--- FRONTEND/src/Admin/Components/DashboardCard.jsx
+++ FRONTEND/src/Admin/Components/DashboardCard.jsx
@@ -1,6 +1,6 @@
 function DashboardCard({ label, value, Icon }) {
   return (
-    <div className="flex h-[15vh] min-h-25 w-[45vw] min-w-full flex-col justify-between rounded-xl bg-neutral-100 p-4 font-[Poppins] text-neutral-800 inset-shadow-med lg:w-1/5">
+    <div className="flex h-[15vh] min-h-25 w-full flex-col justify-between rounded-xl bg-neutral-100 p-4 font-[Poppins] text-neutral-800 inset-shadow-med lg:w-1/5">
       {/** */}
       <span className="w-full text-xs text-neutral-900">{label}</span>
       <span className="flex w-full items-center justify-between gap-y-2">
