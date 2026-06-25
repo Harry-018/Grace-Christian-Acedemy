02738953591f33c92bd29512678ee8cf90a6dc71
Gargallo-Honesto <rafaelgargallo018@gmail.com>
2026-06-25 15:02:29 +0800

    transpo card updated

--- FRONTEND/src/Components/TranspoCard.jsx
+++ FRONTEND/src/Components/TranspoCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const TransopoCard = () => {
+const TranspoCard = () => {
   return <div>TransopoCard</div>;
 };
 
-export default TransopoCard;
+export default TranspoCard;
