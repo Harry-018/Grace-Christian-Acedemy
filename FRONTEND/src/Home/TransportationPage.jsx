273343126bc735e701bb4b3b444e997573bbc413
Gargallo-Honesto <rafaelgargallo018@gmail.com>
2026-06-25 13:52:19 +0800

    first commit on transpopage

--- FRONTEND/src/Home/TransportationPage.jsx
+++ FRONTEND/src/Home/TransportationPage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const TransportationPage = () => {
-  return <div>TransportationPage</div>;
+  return (
+    <div>
+      {/* upperdiv */}
+      <div className="flex flex-col gap-y-5 p-5">
+        <h1 className="font-[PoppinsBold] text-swamp-green">Transportation</h1>
+        <span className="flex w-full justify-between gap-x-5 rounded-2xl bg-bone p-5 text-xs text-ashlight inset-shadow-med">
+          <h2>Pickup and drop-off included</h2>
+          <span className="w-0.5 bg-ashlight"></span>
+          <h2>Scheduled twice a day</h2>
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default TransportationPage;
